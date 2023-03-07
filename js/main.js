1a0ef41fadd49df464509973d978628c72dd6c24
easygode <bimalbenvenuti@gmail.com>
2023-03-07 19:02:48 +0100

    milestone 2 - stampa a console

--- js/main.js
+++ js/main.js
@@ -2,40 +2,56 @@
 //MILESTONE 0 - 1. creo un array con le informazioni fornite
 
 const team = [
-    'a_person', {
+    {
         firstName: 'Wayne',
         lastName: 'Barnett',
         position: 'Founder & CEO',
         img: 'wayne-barnett-founder-ceo.jpg'
     },
-    'b_person', {
+    {
         firstName: 'Angela',
         lastName: 'Caroll',
         position: 'Chief Editor',
         img: 'angela-caroll-chief-editor.jpg'
     },
-    'c_person', {
+    {
         firstName: 'Walter',
         lastName: 'Gordon',
         position: 'Office Manager',
         img: 'walter-gordon-office-manager.jpg'
     },
-    'd_person', {
+    {
         firstName: 'Angela',
         lastName: 'Lopez',
         position: 'Social Media Managr',
         img: 'angela-lopez-social-media-manager.jpg'
     }, 
-    'e_person', {
+   {
         firstName: 'Scott',
         lastName: 'Estrada',
         position: 'Developer',
         img: 'scott-estrada-developer.jpg'
     }, 
-    'f_person', {
+    {
         firstName: 'Barbara',
         lastName: 'Ramos',
         position: 'Graphic Designer',
         img: 'barbara-ramos-graphic-designer.jpg'
     }
 ];
+
+//MILESTONE 1 - 2. stampo a console per ogni membro del team le informazioni di nome, ruolo e la stringa della foto
+
+// prova con una persona
+// console.log (team[0].firstName);
+// console.log (team[0].lastName);
+// console.log (team[0].position);
+// console.log (team[0].img);
+
+for (let i = 0; i < team.length; i++) {
+    console.log(`Staff Member ${i + 1}: `);
+    console.log(`First Name:${team[i].firstName}`);
+    console.log(`Last Name:${team[i].lastName}`);
+    console.log(`Position:${team[i].position}`);
+    console.log(`Picture:${team[i].img}`);
+}
