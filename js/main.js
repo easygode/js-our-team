68565bce4abed7ce1fa3e15c0340d853685f5e54
easygode <bimalbenvenuti@gmail.com>
2023-03-07 18:04:20 +0100

    milestone 0 - array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 'use strict';
+//MILESTONE 0 - 1. creo un array con le informazioni fornite
+
+const team = [
+    'a_person', {
+        firstName: 'Wayne',
+        lastName: 'Barnett',
+        position: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+    'b_person', {
+        firstName: 'Angela',
+        lastName: 'Caroll',
+        position: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+    'c_person', {
+        firstName: 'Walter',
+        lastName: 'Gordon',
+        position: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg'
+    },
+    'd_person', {
+        firstName: 'Angela',
+        lastName: 'Lopez',
+        position: 'Social Media Managr',
+        img: 'angela-lopez-social-media-manager.jpg'
+    }, 
+    'e_person', {
+        firstName: 'Scott',
+        lastName: 'Estrada',
+        position: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    }, 
+    'f_person', {
+        firstName: 'Barbara',
+        lastName: 'Ramos',
+        position: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
